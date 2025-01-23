334f83f76d8c3a645401295491fe30e1d27fa1a5
firstrow <firstrow@gmail.com>
2025-01-23 20:36:01 +0100

    fixed deleting line

--- core.go
+++ core.go
@@ -12,6 +12,7 @@ func lineJoinNext(buf *Buffer, line *Element[Line]) {
 		return
 	}
 	line.Value = append(line.Value, next.Value...)
+	buf.Lines.Remove(next)
 }
 
 func Do(e *Editor, fn func(buf *Buffer, line *Element[Line])) {
