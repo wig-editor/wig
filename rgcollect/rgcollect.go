6e5255d138524ab4e9082d991df3683ecc416905
firstrow <firstrow@gmail.com>
2025-11-03 11:41:52 +0100

    temp

---
+++ rgcollect/rgcollect.go
@@ -0,0 +1,58 @@
+package rgcollect
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/firstrow/wig"
+)
+
+func Init(ctx wig.Context) {
+	buf := wig.NewBuffer()
+	buf.FilePath = "[collect]"
+	ctx.Editor.Buffers = append(ctx.Editor.Buffers, buf)
+	wig.EditorInst.ActiveWindow().ShowBuffer(buf)
+
+	buf.Highlighter = &TestHighlighter{}
+
+	buf.KeyHandler = wig.DefaultKeyHandler(wig.ModeKeyMap{
+		wig.MODE_INSERT: wig.KeyMap{
+			"Enter": func(ctx wig.Context) {
+			},
+		},
+		wig.MODE_NORMAL: wig.KeyMap{
+			"Enter": func(ctx wig.Context) {
+			},
+		},
+	})
+
+	cmd := exec.Command("ls", "-la")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+	}
+	wig.TextInsert(buf, buf.Lines.First(), 0, string(output))
+}
+
+type TestHighlighter struct{}
+
+func (h *TestHighlighter) Build() {
+}
+
+func (h *TestHighlighter) TextChanged(wig.EventTextChange) {
+}
+
+func (h *TestHighlighter) ForRange(startLine, endLine uint32) *wig.HighlighterCursor {
+	nodes := wig.List[wig.HighlighterNode]{}
+	nodes.PushBack(wig.HighlighterNode{
+		NodeName:  "constant",
+		StartLine: 0,
+		StartChar: 2,
+		EndLine:   0,
+		EndChar:   4,
+	})
+	return &wig.HighlighterCursor{
+		Cursor: nodes.First(),
+	}
+}
+
